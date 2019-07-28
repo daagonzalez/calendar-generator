b3eb33c55fb816b149b207a77ff252b35e5c30e5
Daniel Gonzalez <daa.gonzalez@gmail.com>
2019-07-28 17:25:59 -0500

    Added cards for events.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,5 +15,30 @@ export class AppComponent {
     '10',
     '11',
     '12'
-  ]
+  ];
+
+  eventList = [{
+    date: '26 MAR',
+    subject: 'Español',
+    startTime: '7 am',
+    endTime: '9 am',
+    schoolYear: '10',
+    comments: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    date: '27 MAR',
+    subject: 'Matemática',
+    startTime: '7 am',
+    endTime: '9 am',
+    schoolYear: '10',
+    comments: ''
+  },
+  {
+    date: '28 MAR',
+    subject: 'Estudios Sociales',
+    startTime: '7 am',
+    endTime: '9 am',
+    schoolYear: '10',
+    comments: ''
+  }];
 }
